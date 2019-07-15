a2f5c9c04e14793c1d55981f3b6e88a1e12eca03
Calemi <lemzsteamerson@gmail.com>
2019-07-15 14:24:54 -0400

    Start of Sledgehammers!

--- src/main/java/calemiutils/init/InitItems.java
+++ src/main/java/calemiutils/init/InitItems.java
@@ -22,13 +22,19 @@ public class InitItems {
     public static final Item GOLD_CHIP = new ItemBase("gold_chip").addItem();
     public static final Item MOTOR = new ItemBase("motor").addItem();
 
-    public static final Item WALLET = new ItemWallet();
-
     public static final Item SECURITY_WRENCH = new ItemSecurityWrench();
+
+    public static final Item SLEDGEHAMMER_WOOD = new ItemSledgehammer("wood", Item.ToolMaterial.WOOD, 1, 2, 1000);
+    public static final Item SLEDGEHAMMER_STONE = new ItemSledgehammer("stone", Item.ToolMaterial.STONE, 1, 2, 1000);
+    public static final Item SLEDGEHAMMER_IRON = new ItemSledgehammer("iron", Item.ToolMaterial.IRON, 1, 2, 1000);
+    public static final Item SLEDGEHAMMER_GOLD = new ItemSledgehammer("gold", Item.ToolMaterial.GOLD, 1, 2, 1000);
+    public static final Item SLEDGEHAMMER_DIAMOND = new ItemSledgehammer("diamond", Item.ToolMaterial.DIAMOND, 1, 2, 1000);
 
     public static final Item PENCIL = new ItemPencil();
     public static final Item BRUSH = new ItemBrush();
     public static final Item ERASER = new ItemEraser();
+
+    public static final Item WALLET = new ItemWallet();
 
     public static final Item BUILDERS_KIT = new ItemBuildersKit();
 
